9edceec3ec5d5f4378c5fd1112e27fc2c5de74a0
Jakob Fischer <JakobFischer93@gmail.com>
2018-11-19 21:00:37 +0100

    Changed some stuff.

--- rule30.py
+++ rule30.py
@@ -11,6 +11,7 @@ class Rule30:
         self.size = size
         self.cells = np.full(size, False)
         self.cells[round(self.size / 2)] = True
+        self.history = []
         self.run(dry_runs)
 
     def run(self, count):
@@ -18,25 +19,22 @@ class Rule30:
             self.tick()
 
     def tick(self):
-        next_cells = np.empty(len(self.cells), dtype = bool)
+        next_cells = np.empty_like(self.cells)
         for i in range(len(self.cells)):
-            if i - 1 >= 0:
-                left = self.cells[i - 1]
-            else:
-                left = self.cells[len(self.cells) - 1]
-            if i + 1 < len(self.cells):
-                right = self.cells[i + 1]
-            else:
-                right = self.cells[0]
+            left = self.cells[(i - 1) % self.size]
+            right = self.cells[(i + 1) % self.size]
             center = self.cells[i]
             next_cells[i] = left ^ (center | right)
+        self.history.append(self.cells)
+        if len(self.history) > 2:
+            self.history = self.history[1:]
         self.cells = next_cells
 
     def create_image(self, height, img_name):
         start_time = time.perf_counter()
         img = np.empty((height, self.size))
+        f = lambda e: 0xFF if e else 0
         for i in range(height):
-            f = lambda v: 0xFF if v else 0
             img[i, :] = np.array([f(e) for e in self.cells])
             self.tick()
             if i % int(height / 10) == 0:
